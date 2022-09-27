723f148b9e20d077e89ec656bbcc8cda92fb14d3
raylaijh <32132864+raylaijh@users.noreply.github.com>
2022-09-27 15:59:30 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,10 +15,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
+  cluster_name = "demo-eks-cluster"
 }
 
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
+
